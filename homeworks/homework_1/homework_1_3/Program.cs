bde09ee3fceca052ae95c7c9df8cb2cf5a985e64
Ruslan Kharitonov <=>
2022-07-03 18:02:10 +0300

    add homework 1.3 is ready

--- homeworks/homework_1/homework_1_3/Program.cs
+++ homeworks/homework_1/homework_1_3/Program.cs
@@ -1,14 +1,6 @@
 ﻿Console.Clear();
-Console.Write("Input first number: ");
-int number_1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input second number: ");
-int number_2 = Convert.ToInt32(Console.ReadLine());
-int max;
-if (number_1 > number_2){
-    max = number_1;
-    Console.Write("a = " + number_1 + "; b = " + number_2 + " -> max =" + max);
-}
-else{
-    max = number_2;
-    Console.Write("a = " + number_1 + "; b = " + number_2 + " -> max = " + max);
-}
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0) Console.Write(number + " -> " + "да");
+else Console.Write(number + " -> " + "нет");
