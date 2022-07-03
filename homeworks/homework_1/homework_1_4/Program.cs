fb40350be9f3cdb7e83afe1556569850c7b82db9
Ruslan Kharitonov <=>
2022-07-03 18:49:52 +0300

    add homework 1.4 is done

--- homeworks/homework_1/homework_1_4/Program.cs
+++ homeworks/homework_1/homework_1_4/Program.cs
@@ -1,14 +1,11 @@
 ﻿Console.Clear();
 Console.Write("Input first number: ");
-int number_1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input second number: ");
-int number_2 = Convert.ToInt32(Console.ReadLine());
-int max;
-if (number_1 > number_2){
-    max = number_1;
-    Console.Write("a = " + number_1 + "; b = " + number_2 + " -> max =" + max);
-}
-else{
-    max = number_2;
-    Console.Write("a = " + number_1 + "; b = " + number_2 + " -> max = " + max);
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 1;
+Console.Write(number + " -> ");
+for (int index = 1; index <= number; index++)
+{
+    if (sum % 2 == 0) Console.Write(sum);
+    if ((sum % 2 == 0) & (sum < number-1)) Console.Write(", ");
+    sum++;
 }
