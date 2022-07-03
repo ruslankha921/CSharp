c3dd0f7b262f237fe74f1e45d3c643fcc390f425
Ruslan Kharitonov <=>
2022-07-03 17:38:29 +0300

    add all paths in pero

---
+++ Examples/Example010_MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] array = { 1, 21, 31, 43, 53, 69, 73, 88 };
+
+int n = array.Length;
+int find = 21;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++;
+}
---
+++ lessons/seminars/seminar_1/Program.cs
@@ -0,0 +1,39 @@
+﻿// //запрашиваем у пользователя число
+// Console.Write("Input number: ");
+
+// // создаем переменную number и сохраняем в нее значение, введенное из консоли
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// //объявляем переменную q и сохраняем в нее квадрат числа number
+// int q = number * number;
+
+// //выыодим значение переменных number и 
+// Console.WriteLine(number + " -> " + q);
+
+//Напишите программу, 
+// a = 25, b = 5 -> да
+
+//вводим первое число
+Console.Write("Input first number: ");
+
+
+//сохраняем значение в переменную
+int number_1 = Convert.ToInt32(Console.ReadLine());
+
+//вводим второе число
+
+Console.Write("Input first number: ");
+
+//сохраняем значение в переменную
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+// объявляем переменную q, в которую сохраняю  
+int q = number_2 * number_2;
+
+//сравниваем значение первой переменной, со значением второй переменной, умноженной на самого себя number_2 * number_2 числа
+if (number_1 == q){
+    Console.Write("a = " + number_1 + ", b = " + number_2 + " -> " + " yes");
+}
+else{
+      Console.Write("a = " + number_1 + ", b = " + number_2 + " -> " + " no");
+}
